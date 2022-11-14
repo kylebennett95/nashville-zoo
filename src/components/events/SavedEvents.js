62f148ddc495e84341facc3adb4b8aa2e8aa77f2
DeminZawity <106280855+DeminZawity@users.noreply.github.com>
2022-11-13 21:25:33 -0600

    started the saved events page

--- src/components/events/SavedEvents.js
+++ src/components/events/SavedEvents.js
@@ -1,6 +1,21 @@
 import React from "react";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export const SavedEvents = () => {
+  const [savedEvents, setSavedEvents] = useState([])
+
+  const localProjectUser = localStorage.getItem("project_user");
+  const projectUserObject = JSON.parse(localProjectUser);
+
+  useEffect(() => {
+    fetch(`http://localhost:8088/attractions`)
+      .then((response) => response.json())
+      .then((eventArray) => {
+        setSavedEvents(eventArray);
+      });
+  }, []);
+
   return (
     <>
       <h2>Saved Events</h2>
